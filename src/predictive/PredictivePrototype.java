77f9aa0106ee7fadf671976db2c9a040bb3b7027
Charles Chambers <cxc320@bham.ac.uk>
2014-02-28 21:23:03 +0000

    Updating files

--- src/predictive/PredictivePrototype.java
+++ src/predictive/PredictivePrototype.java
@@ -9,15 +9,20 @@ public class PredictivePrototype {
     public static String wordToSignature(String word) {
         // Using StringBuffer rather than String is a more efficient solution as it allows a single variable to be appended.
         // This therefore means it performs the concatenations much more effectively.
-        
-    	StringBuffer buffer = new StringBuffer();
+
+        // Creates new string buffer to hold the signature.
+        StringBuffer buffer = new StringBuffer();
+
+        // Converts the word to lower case.
         word = word.toLowerCase();
 
+        // Loops through each letter in the word.
         for (int count = 0; count < word.length(); count++) {
             char letter = word.charAt(count);
 
+            // Compares the char with each case and appends corresponding number to buffer.
             switch (letter) {
-                case 'a': case 'b': case 'c':
+                case 'a':case 'b':case 'c':
                     buffer.append(2);
                     break;
                 case 'd':case 'e':case 'f':
@@ -41,11 +46,13 @@ public class PredictivePrototype {
                 case 'w':case 'x':case 'y':case 'z':
                     buffer.append(9);
                     break;
+                // If the char is not in the alphabet, simply append a blank space.
                 default:
                     buffer.append(" ");
                     break;
             }
         }
+        // Return the string version of the buffer.
         return buffer.toString();
     }
 
@@ -53,27 +60,34 @@ public class PredictivePrototype {
         // Each time a word is being input, the system has to read through the dictionary line by line.
         // This is obviously hugely inefficient, especially using a Scanner to read the data.
         // A better solution would be to initially store the dictionary in a hash set then continuously refer to the set.
-    	
+
+        // Initialises a new Scanner.
         Scanner filescan = null;
+
+        // Tries to scan in the dictionary, else catches the exception.
         try {
             filescan = new Scanner(new File("/Users/charliechambers/Documents/University First Year/Software Workshop/Programming/PredictiveTextEntry/src/predictive/dictionary.txt"));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
+        // Creates a new hash set to store the results.
         Set<String> results = new HashSet<String>();
 
+        // While the scanner has an input.
         while (filescan.hasNext()) {
+            // The scanners input is saved as a string and converted to lower case.
             String current = (filescan.nextLine().toLowerCase());
-            if (wordToSignature(current).equals(signature)) {
-                if (!(results.contains(current))) {
-                    results.add(current);
-                }
+            // If current signature is equal to the signature being passed in and set containing the results does not already contain the word.
+            if (wordToSignature(current).equals(signature) && !(results.contains(current))) {
+                // Add the word to the results set.
+                results.add(current);
             }
-
         }
         return results;
     }
+
 }
+
 
 

883d0b9aec87e60cfe65f00f83ac23486656bc8c
Charles Chambers <cxc320@bham.ac.uk>
2014-03-06 21:17:46 +0000

    Updating files

--- src/predictive/DictionaryListImpl.java
+++ src/predictive/DictionaryListImpl.java
@@ -1,7 +1,42 @@
 package predictive;
 
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Created by Charles Chambers on 06/03/2014.
  */
 public class DictionaryListImpl {
+
+    private FileInputStream textFile;
+    private BufferedReader readWords;
+    private ArrayList<WordSig> arrayWordList = new ArrayList<WordSig>();
+
+
+    public DictionaryListImpl() {
+        try {
+            textFile = new FileInputStream("/usr/share/dict/words");
+            readWords = new BufferedReader(new InputStreamReader(textFile));
+            String line = readWords.readLine();
+            while (line != null) {
+                arrayWordList.add(new WordSig(line));
+                line = readWords.readLine();
+            }
+            textFile.close();
+        } catch (Exception e) {
+            System.out.println("SHIT!");
+        }
+    }
+
+    public static void main(String[] args) {
+        DictionaryListImpl test = new DictionaryListImpl();
+        Collections.sort(test.arrayWordList);
+        for (WordSig trial : test.arrayWordList) {
+            System.out.println(trial.toString());
+        }
+    }
 }
---
+++ src/predictive/WordSig.java
@@ -0,0 +1,50 @@
+package predictive;
+
+/**
+ * Created by Charles Chambers on 06/03/2014.
+ */
+public class WordSig implements Comparable<WordSig> {
+
+    private String signature;
+    private String word;
+
+    public WordSig(String word) {
+        this.word = word;
+        this.signature = PredictivePrototype.wordToSignature(word);
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    @Override
+    public String toString() {
+        return "WordSig{" + "word='" + word + '\'' + ", signature='" + signature + '\'' + '}';
+    }
+
+    @Override
+    public int compareTo(WordSig wordSig) {
+        if (signature.length() < wordSig.getSignature().length()) {
+            return -1;
+        } else if (signature.length() > wordSig.getSignature().length()) {
+            return 1;
+        } else {
+            for (int i = 0; i < signature.length(); i++) {
+                char current = signature.charAt(i);
+                char test = wordSig.getSignature().charAt(i);
+                if (current < test) {
+                    return -1;
+                }
+                if (current > test) {
+                    return 1;
+                }
+            }
+        }
+        return 0;
+    }
+}
+

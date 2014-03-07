22e63a741f934a88d0a06f902893c9392a2e213a
Charles Chambers <cxc320@bham.ac.uk>
2014-03-07 01:10:36 +0000

    Update

--- src/predictive/Sigs2WordsList.java
+++ src/predictive/Sigs2WordsList.java
@@ -2,11 +2,11 @@ package predictive;
 import java.util.Set;
 
 public class Sigs2WordsList {
-    private static DictionaryListImpl test = new DictionaryListImpl();
     public static void main(String[] args){
+        DictionaryListImpl test = new DictionaryListImpl();
         for (String curr: args) {
             Set<String> results = test.signatureToWords(curr);
-            System.out.println(curr + ": " + results.toString());
+            System.out.println(curr + ": " + results);
         }
     }
 }
--- src/predictive/Sigs2WordsProto.java
+++ src/predictive/Sigs2WordsProto.java
@@ -6,7 +6,7 @@ public class Sigs2WordsProto {
     public static void main(String[] args){
     	for (String curr: args) {
             Set<String> results = PredictivePrototype.signatureToWords(curr);
-        	System.out.println(curr + ": " + results.toString());
+        	System.out.println(curr + ": " + results);
         }
     }
 

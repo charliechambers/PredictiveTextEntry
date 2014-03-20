a2d230a5bd790bb7907fc0eb8385e86b2c608ec1
Charles Chambers <cxc320@bham.ac.uk>
2014-03-20 11:50:18 +0000

    Update

--- src/predictive/Sigs2WordsMap.java
+++ src/predictive/Sigs2WordsMap.java
@@ -2,17 +2,17 @@ package predictive;
 import java.util.Set;
 
 // Time whilst executing on just one value, 4663:
-// real	0m0.982s
+// real	0m0.822s
 
 // Time whilst executing on multiple values, 27753 672643 62646 73224 7327 47273.
-// real	0m0.974s
+// real	0m0.816s
 
 /**
  * Created by Charles Chambers on 18/03/2014.
  */
-public class Sigs2WordsTree {
+public class Sigs2WordsMap {
     public static void main(String[] args) {
-        Dictionary test = new DictionaryTreeImpl("/usr/share/dict/words");
+        Dictionary test = new DictionaryMapImpl();
         for (String curr : args) {
             Set<String> results = test.signatureToWords(curr);
             System.out.println(curr + ": " + results);
